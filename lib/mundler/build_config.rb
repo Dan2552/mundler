c00917ac78097baec29bf2c1ad8d0588a50142ed
Daniel Inkpen <dan2552@gmail.com>
2020-09-25 18:56:39 +0100

    Fix iOS min version; also fixing builds for Xcode 12

--- lib/mundler/build_config.rb
+++ lib/mundler/build_config.rb
@@ -9,9 +9,8 @@ if `which xcrun`.chomp.strip.length > 0
   clang_path = `xcrun -find clang`.chomp
   ios_sdk = `xcrun --sdk iphoneos --show-sdk-path`.chomp
   ios_simulator_sdk = `xcrun --sdk iphonesimulator --show-sdk-path`.chomp
-  base_flags = ["-O3", "-mios-simulator-version-min=#{min_ver}"]
-  ios_flags = base_flags + %W(-arch armv7 -arch arm64 -isysroot #{ios_sdk})
-  ios_simulator_flags = base_flags + %W(-arch i386 -arch x86_64 -isysroot #{ios_simulator_sdk})
+  ios_flags = %W(-O3 -miphoneos-version-min=#{min_ver} -arch armv7 -arch arm64 -isysroot #{ios_sdk})
+  ios_simulator_flags = %W(-O3 -mios-simulator-version-min=#{min_ver} -arch i386 -arch x86_64 -isysroot #{ios_simulator_sdk})
 
   MRuby::CrossBuild.new('ios') do |conf|
     {{ contents }}
